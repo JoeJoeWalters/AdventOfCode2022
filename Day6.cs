4a3a78ce692a112e3e51ab097b67a56546350bc4
Joe Walters <info@thenakeddeveloper.com>
2022-12-05 20:52:18 +0000

    -

---
+++ Day6.cs
@@ -0,0 +1,14 @@
+namespace AdventOfCode2022
+{
+    public class Day6
+    {
+        /*
+        */
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Day7.cs
@@ -0,0 +1,14 @@
+namespace AdventOfCode2022
+{
+    public class Day7
+    {
+        /*
+        */
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

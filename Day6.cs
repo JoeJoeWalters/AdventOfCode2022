37a9a3f606c3e805f95d27603aee7f727f91ecba
Joe Walters <N16217@newday.co.uk>
2022-12-06 16:21:42 +0000

    Day 6

--- Day6.cs
+++ Day6.cs
@@ -9,13 +9,28 @@ namespace AdventOfCode2022
         public void Test()
         {
             // ARRANGE
-            string streamData = File.ReadAllText(@".\Day6.txt");
+            char[] streamData = File.ReadAllText(@".\Day6.txt").ToCharArray();
+            string receivedData = String.Empty; // Nothing is received at the start
+            int markerLength = 14; // Part 1 = 4, Part 2 = 14
 
-            // Stream each character
-            foreach (char c in streamData)
+            // Stream to the elves once char at a time (we could cheat but lets not)
+            for (int i = 0; i < streamData.Length; i++)
             {
+                char c = streamData[i];
+                receivedData = $"{receivedData}{c}";
 
+                // Take the last X characters if they is enough
+                if (receivedData.Length > markerLength)
+                {
+                    char[] chars = receivedData.Substring(receivedData.Length - markerLength, markerLength).ToCharArray();
+                    bool distinct = chars.Take(markerLength).Distinct().Count() == markerLength;
+                    if (distinct)
+                    {
+                        var answer = i + 1;
+                    }
+                }
             }
+
         }
     }
 }
